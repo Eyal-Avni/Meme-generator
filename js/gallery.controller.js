ded6e13869d6d35786635afe4a8a3fc3d30caffe
Eyal-Avni <67070652+Eyal-Avni@users.noreply.github.com>
2023-04-25 21:18:46 +0300

    added routing between sections

--- js/gallery.controller.js
+++ js/gallery.controller.js
@@ -15,5 +15,46 @@ function renderGallery() {
 function onImgSelect(imgIdx) {
     const img = findImgByIdx(imgIdx)
     setSelectedImgId(img)
+    routeToEditor()
     renderMeme()
+}
+
+function onRoute(route) {
+    toggleNavButtons(route)
+    hideSections()
+    openTab(route)
+}
+
+function routeToEditor() {
+    document.querySelector('.gallery-container').classList.add('none')
+    document.querySelector('.editor-container').classList.remove('none')
+    document
+        .querySelector('.main-menu .btn-gallery')
+        .parentElement.classList.remove('active')
+    toggleNavButtons(null)
+}
+
+function hideSections() {
+    document.querySelector('.gallery-container').classList.add('none')
+    document.querySelector('.editor-container').classList.add('none')
+}
+
+function openTab(route) {
+    document.querySelector(`.${route}-container`).classList.remove('none')
+}
+
+function toggleNavButtons(route) {
+    if (!route) return
+    document
+        .querySelector('.main-menu .btn-gallery')
+        .parentElement.classList.remove('active')
+    document
+        .querySelector('.main-menu .btn-memes')
+        .parentElement.classList.remove('active')
+    document
+        .querySelector('.main-menu .btn-about')
+        .parentElement.classList.remove('active')
+    document
+        .querySelector(`.main-menu .btn-${route}`)
+        .parentElement.classList.add('active')
 }
